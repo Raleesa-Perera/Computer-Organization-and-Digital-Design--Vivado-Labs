7394fb17fd1cf302b7c78d5bdd85abd2a5d2726e
Raleesa-Perera <162663755+Raleesa-Perera@users.noreply.github.com>
2024-03-22 20:21:49 +0530

    Update HA.vhd

--- Week-04-Adder/HA.vhd
+++ Week-04-Adder/HA.vhd
@@ -1,1 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/07/2024 11:49:04 PM
+-- Design Name: 
+-- Module Name: HA - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity HA is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           S : out STD_LOGIC;
+           C : out STD_LOGIC);
+end HA;
+
+architecture Behavioral of HA is
+
+signal S_1 : std_logic;
+signal S_2 : std_logic;
+
+begin
+    S_1 <= (NOT A) AND B;
+    S_2 <= A AND (NOT B);
+    S <= S_1 OR S_2;
+    
+    C <= A AND B;
+
+
+end Behavioral;
 
